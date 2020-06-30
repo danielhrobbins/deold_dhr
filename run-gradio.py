5c76d2ecdf37d2b0019340488b39a1ca6598715a
Abubakar Abid <a12d@stanford.edu>
2020-06-30 15:20:26 -0500

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -18,18 +18,16 @@ WATERMARK_LINK = 'https://media.githubusercontent.com/media/jantic/DeOldify' \
 WATERMARK_PATH = './resource_images/watermark.png'
 
 
-def load():
-    if not os.path.exists('models'):
-        os.makedirs('models')
-    if not os.path.exists(COLORIZE_ARTISTIC_PATH):
-        wget.download(COLORIZE_ARTISTIC_LINK, COLORIZE_ARTISTIC_PATH)
-    if not os.path.exists(WATERMARK_PATH):
-        wget.download(WATERMARK_LINK, WATERMARK_PATH)
-    colorizer = get_image_colorizer(artistic=True)
-    return colorizer
+if not os.path.exists('models'):
+    os.makedirs('models')
+if not os.path.exists(COLORIZE_ARTISTIC_PATH):
+    wget.download(COLORIZE_ARTISTIC_LINK, COLORIZE_ARTISTIC_PATH)
+if not os.path.exists(WATERMARK_PATH):
+    wget.download(WATERMARK_LINK, WATERMARK_PATH)
+colorizer = get_image_colorizer(artistic=True)
 
 
-def predict(inp, colorizer):
+def predict(inp):
     render_factor = 35
     watermarked = False
     result = colorizer.plot_transformed_image_from_pil(inp,
