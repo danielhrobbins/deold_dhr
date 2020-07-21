688544f436eb19e2071618d32d33e5d22829e0ed
aliabd <ali.si3luwa@gmail.com>
2020-07-21 03:00:02 -0700

    added latency

--- run-gradio.py
+++ run-gradio.py
@@ -43,6 +43,12 @@ examples = [
 ["examples/sinatra-jfk.jpg"],
 ["examples/payphone.png"]
 ]
-INTERFACE = gradio.Interface(fn=predict, inputs=INPUTS, outputs=OUTPUTS, title='De-Oldify', description='De-oldify colorizes old images. Try it out on an old image you want restored! Note: It will take some time to process the image.', thumbnail='https://i2.wp.com/www.marktechpost.com/wp-content/uploads/2019/08/68747470733a2f2f692e696d6775722e636f6d2f427430766e6b652e6a7067.jpg?fit=1182%2C768&ssl=1')
+INTERFACE = gradio.Interface(fn=predict, inputs=INPUTS, outputs=OUTPUTS,
+                             title='De-Oldify', description='De-oldify '
+                                                            'colorizes old '
+                                                            'images. Try it '
+                                                            'out on an old '
+                                                            'image you want '
+                                                            'restored! Note: It takes about 30s to process the image.', thumbnail='https://i2.wp.com/www.marktechpost.com/wp-content/uploads/2019/08/68747470733a2f2f692e696d6775722e636f6d2f427430766e6b652e6a7067.jpg?fit=1182%2C768&ssl=1')
 
 INTERFACE.launch(inbrowser=True)
