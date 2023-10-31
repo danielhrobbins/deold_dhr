07b1d2b3b095ab1a8b888cee84b6d9da4d66c9f8
danielhrobbins <danielhrobbins@gmail.com>
2023-10-31 08:08:26 -0500

    Update visualize.py

--- deoldify/visualize.py
+++ deoldify/visualize.py
@@ -65,8 +65,8 @@ class ModelImageVisualizer:
     def plot_transformed_image_from_url(
         self,
         url: str,
-        path: str = 'test_images/image.png',
-        results_dir:Path = None,
+        path: str = 'test_images/image01.png',
+        results_dir:Path = (./results_dir),
         figsize: Tuple[int, int] = (20, 20),
         render_factor: int = None,
         
@@ -91,7 +91,7 @@ class ModelImageVisualizer:
     def plot_transformed_image(
         self,
         path: str,
-        results_dir:Path = None,
+        results_dir:Path = (./results_dir),
         figsize: Tuple[int, int] = (20, 20),
         render_factor: int = None,
         display_render_factor: bool = False,
